5b634a3922ffe1196c5475f19bb50548915f8f66
alexUd01 <alexanderikpeama@gmail.com>
2023-03-31 14:29:43 +0100

    re-initialized repository after deletion

---
+++ 0x11-python-network_1/practice/1-request_basics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import requests
+"""Behold the power of Requests"""
+
+r = requests.get('https://api.github.com/users/alexUd01', auth=('user', 'pass'))
+print(r.status_code)
+print(r.headers['content-type'])
+print(r.encoding)
+print(r.text)
+print(r.json())
