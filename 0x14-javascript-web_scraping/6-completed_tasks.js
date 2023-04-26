50cd0c724e16aa73f6c01613ce92197a7ed3b6c9
alexUd01 <alexanderikpeama@gmail.com>
2023-04-26 04:00:49 +0100

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,15 +9,24 @@ const request = require('request');
 
 const myFunc = function (_err, _res, body) {
   body = JSON.parse(body);
-  result = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0,
-	    '6': 0, '7': 0, '8': 0, '9': 0, '10': 0};
-  count = 0;
+  const result = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0
+  };
   for (const task of body) {
     if (task.completed) {
       result[task.userId] += 1;
     }
   }
   console.log(result);
-}
+};
 
-request(process.argv[2], myFunc)
+request(process.argv[2], myFunc);
